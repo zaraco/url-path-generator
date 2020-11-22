bc4b000afb2c8df3329ec7ed3498525056968be1
Zahra Teymouri <zahrateymouri90@gmail.com>
2020-11-22 15:27:51 +0100

    v1.1.0

--- README.md
+++ README.md
@@ -7,11 +7,12 @@ npm install url-path-generator
 ```
 
 ```javascript
-const path = new Path('http://zara-co.com/')
+import urlPathGenerator from 'url-path-generator';
+const example = urlPathGenerator.init('http://zara-co.com/')
     .addToPath('users', "550e8400-e29b-41d4-a716-446655440000")
     .addToPath('messages', "550e8400-e29b-41d4-a716-446655440000")
     .addToPath('attachments')
     .build();
-console.log(path);
+console.log(example);
 // it logs http://zara-co.com/users/550e8400-e29b-41d4-a716-446655440000/messages/550e8400-e29b-41d4-a716-446655440000/attachments
 ```
--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-module.exports = class Path {
-    baseUrl;
-    pathArray = [];
-    constructor(baseUrl) {
-        this.baseUrl = baseUrl
-    }
-    addToPath = (entity, id = null) => {
-        if(entity) {
-            this.pathArray.push(entity);
-            if(id) {
-                this.pathArray.push(id);
-            }
-        }
-        return this;
-    };
-    build = () => {
-        if(this.baseUrl.slice(-1) === '/') {
-            this.baseUrl = this.baseUrl.substring(0, this.baseUrl.length - 1);
-        }
-        return this.baseUrl + this.pathArray.map(value => `/${value}`).join('')
-    }
+exports = module.exports;
+
+exports.baseUrl = '';
+exports.pathArray = [];
+
+exports.init = (baseUrl) =>
+{
+    this.pathArray = [];
+    this.baseUrl = baseUrl;
+    return this;
 };
+
+exports.addToPath = (entity, id = null) => {
+    if (entity) {
+        this.pathArray.push(entity);
+        if (id) {
+            this.pathArray.push(id);
+        }
+    }
+    return this;
+};
+exports.build = () => {
+    if (this.baseUrl.slice(-1) === '/') {
+        this.baseUrl = this.baseUrl.substring(0, this.baseUrl.length - 1);
+    }
+    return this.baseUrl + this.pathArray.map(value => `/${value}`).join('')
+};
+
